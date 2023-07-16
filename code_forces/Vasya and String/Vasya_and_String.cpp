902f47a9ee855a81485382c38685f879cae610b5
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-07-15 23:11:13 -0300

    Vasya and String

--- code_forces/Vasya and String/Vasya_and_String.cpp
+++ code_forces/Vasya and String/Vasya_and_String.cpp
@@ -1,65 +1,151 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-int main(){
-    int n, k;
-    string s, newS;
-    vector<int> v;
+int n, k;
+string s;
+int value = 0;
+vector<int> v;
+int mid;
 
+int validate(){
+    int cont = 0, sub = k, start = 0;
+    
+    for(int i = 0; i != v.size(); i++){
+        if(cont >= mid){
+            mid = cont;
+            return 1;
+        }
+
+        if(i % 2 == 0){
+            cont += v[i];
+            continue;
+        }
+
+        sub -= v[i];
+        if(sub >= 0){
+            cont += v[i];
+            continue;
+        }
+
+        cont += v[i] + sub;
+        if(cont >= mid){
+            mid = cont;
+            return 1;
+        }
+        cont -= v[i] + sub;
+        
+        if(k < v[i]){
+            cont = 0;
+            start = i+1;
+            sub = k;
+            continue;
+        }
+
+        while(sub < 0){
+            cont -= v[start];
+            start++;
+            cont -= v[start];
+            sub += v[start];
+            start++;
+        }
+        
+        cont += v[i];
+    }
+    if(start > 0){
+        cont += min(sub, v[start-1]);
+    }
+
+    if(cont >= mid){
+        mid = cont;
+        return 1;
+    }
+    
+    cont = 0; sub = k, start = 1; 
+    for(int i = 1; i != v.size(); i++){
+        if(cont >= mid){
+            mid = cont;
+            return 1;
+        }
+
+        if(i % 2 == 1){
+            cont += v[i];
+            continue;
+        }
+
+        sub -= v[i];
+        if(sub >= 0){
+            cont += v[i];
+            continue;
+        }
+        
+        cont += v[i] + sub;
+        if(cont >= mid){
+            mid = cont;
+            return 1;
+        }
+        cont -= v[i] + sub;
+
+        if(k < v[i]){
+            cont = 0;
+            start = i+1;
+            sub = k;
+            continue;
+        }
+        
+        while(sub < 0){
+            cont -= v[start];
+            start++;
+            cont -= v[start];
+            sub += v[start];
+            start++;
+        }
+
+        cont += v[i];
+    }
+    cont += min(sub, v[start-1]);
+
+    if(cont >= mid){
+        mid = cont;
+        return 1;
+    }
+
+    return 0;
+}
+
+int bs(int low, int high){
+    mid = (low + high)/2;
+
+    if(low > high){
+        return value;
+    }
+
+    if(validate() == 1){
+        value = mid;
+        return bs(mid + 1, high);
+    }
+
+    return bs(low, mid-1);
+}
+
+int main(){
     cin >> n >> k;
     cin >> s;
 
-    int contA = 0, contB = 0;
-    for(int i = 0; i != n; i++){
-        if(s[i] == 'a'){
-            contA++;
+    int cont = 1;
+    for(int i = 1; i != n; i++){
+        if(s[i] == s[i-1]){
+            cont++;
         }else{
-            contB++;
-        }
-
-        if(s[i] == 'a' && contB != 0){
-            v.push_back(contB);
-            contB = 0;
-        }else if(s[i] == 'b' && contA != 0){
-            v.push_back(contA);
-            contA = 0;
+            v.push_back(cont);
+            cont = 1;
         }
     }
 
-    if(contA != 0){
-        v.push_back(contA);
-    }else{
-        v.push_back(contB);
-    }
+    v.push_back(cont);
 
-    int mx = 0;
-    for(int i = 0; i != v.size(); i++){
-        int convert = k, cont = 0;
-        for(int j = i; convert >= 0 && j < v.size(); j++){
-            cont += v[j];
-
-            if(j < v.size()-1 && v[j+1] <= convert){
-                cont += v[j+1];
-                convert -= v[j+1];
-                j++;
-            }else if(j < v.size()-1 && convert != 0){
-                cont += convert;
-                convert = -1;
-                j++;
-            }else{
-                break;
-            }
-        }
-        if(convert > 0 && i > 0){
-        
-            cont += min(v[i-1], convert);
-        }
-   
-        mx = max(mx, cont);
-    }
-
-    cout << mx << endl;
+    cout << bs(k, n) << endl;
 }
