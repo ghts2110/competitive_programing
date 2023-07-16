c0973bca4fce00cd018d4dfdd3ecef6278393eaf
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-07-16 17:25:20 -0300

    Solving for Carrots

---
+++ kattis/Solving for Carrots/Solving_for_Carrots.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n, p;
+    string s;
+
+    cin >> n >> p;
+
+    for(int i = 0; i != n; i++){
+        cin >> s;
+    }
+
+    cout << p << endl;
+}
