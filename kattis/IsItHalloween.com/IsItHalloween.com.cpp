74b9614b49c5d418daf10dea1b61012db5f3e76d
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-07-17 17:13:09 -0300

    IsItHalloween.com

---
+++ kattis/IsItHalloween.com/IsItHalloween.com.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin, s);
+
+    if(s == "OCT 31" || s == "DEC 25"){
+        cout << "yup" << endl;
+    }else{
+        cout << "nope" << endl;
+    }
+}
