d6417c6e4ea46d9a3070d1c5aa00c87349937b63
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-07-19 22:07:05 -0300

    Nasty Hacks

---
+++ kattis/Nasty Hacks/Nasty_Hacks.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n; 
+    cin >> n;
+
+    while(n--){
+        int r, e, c;
+        cin >> r >> e >> c;
+        if(r < e - c){
+            cout << "advertise" << endl;
+        }else if(r == e - c){
+            cout << "does not matter" << endl;
+        }else{
+            cout << "do not advertise" << endl;
+        }
+    }
+}
