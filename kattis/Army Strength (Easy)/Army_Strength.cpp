ac839c75a944eecc34ad6fc610875ac5bad73c5f
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-07-24 11:58:53 -0300

    Army Strength (Easy)

---
+++ kattis/Army Strength (Easy)/Army_Strength.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+#define pb push_back
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int g, m;
+        cin >> g >> m;
+
+        vector<int> god;
+        for(int i = 0; i != g; i++){
+            int num;
+            cin >> num;
+
+            god.pb(num);            
+        }
+
+        vector<int> mec;
+        for(int i = 0; i != m; i++){
+            int num;
+            cin >> num;
+
+            mec.pb(num);
+        }
+
+        while(god.size() != 0 && mec.size() != 0){
+            if(god[0] < mec[0]){
+                god.erase(god.begin());
+            }else{
+                mec.erase(mec.begin());
+            }
+        }
+
+        if(god.size() != 0){
+            cout << "Godzilla" << endl;
+        }else{
+            cout << "MechaGodzilla" << endl;
+        }
+    }
+}
