5a655a14801619a28eeafe8e495d394f9de33d95
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-07-27 23:25:44 -0300

    Howl

---
+++ kattis/Howl/Howl.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    cout << "AWAWHO";
+    for(int i = 0; i <= s.size(); i++){
+        cout << "OO";
+    }
+    cout << endl;
+}
