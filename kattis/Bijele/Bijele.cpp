77ffc6de340910e32dd8ec2f067cda3038573d84
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-08-04 01:46:49 -0300

    Bijele

---
+++ kattis/Bijele/Bijele.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int k, q, t, b, c, p;
+    while(cin >> k >> q >> t >> b >> c >> p){
+        cout << -(k-1) << " " << -(q-1) << " " << -(t-2) << " " <<-(b-2)<<" "<<-(c-2)<<" "<<-(p-8)<<endl;
+    }
+}
