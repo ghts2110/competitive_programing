15044d3a464ea4955c22d0bb355dafc0a3bd1fbe
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-08-11 21:39:00 -0300

    GCD

---
+++ uva/GCD/GCD.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b){
+    if(b == 0){
+        return a;
+    }
+
+    return gcd(b, a%b);
+}
+
+int main(){
+    int n;
+    while(cin >> n){
+        if(n == 0){
+            break;
+        }
+
+        long long g = 0;
+        for(int i = 1; i != n; i++){
+            for(int j = i+1; j <= n; j++){
+                g+= gcd(i, j);
+            }
+        }
+
+        cout << g << endl;
+    }
+}
