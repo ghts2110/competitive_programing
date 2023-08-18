dbd8f01575a151d50bf89fd4f8bc4a8bfc8b391f
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-08-18 19:35:14 -0300

    Jus the Facts

---
+++ uva/Jus the Facts/Jus_the_Facts.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num;
+        while (sc.hasNext()) {
+            num = sc.nextInt();
+            BigInteger cont = BigInteger.ONE;
+            for (int c = 2; c <= num; c++) {
+                cont = cont.multiply(BigInteger.valueOf(c));
+                while (cont.mod(BigInteger.TEN).equals(BigInteger.ZERO)) {
+                    cont = cont.divide(BigInteger.TEN);
+                }
+            }
+
+            String contStr = cont.toString();
+            char asw = contStr.charAt(contStr.length() - 1);
+
+            System.out.printf("%5d -> %c%n", num, asw);
+        }
+    }
+}
+
