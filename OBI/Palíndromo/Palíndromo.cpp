0e7ff7b9c076e8c3f323a17da6e70dea43d744fc
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-11-22 21:35:24 -0300

    Palíndromo

---
+++ OBI/Palíndromo/Palíndromo.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void bfs(vector<vector<int> > &v, int start, vector<int> &dist){
+    dist[start] = 0;
+    queue<int> q;
+    q.push(0);
+
+    while(!q.empty()){
+        int no = q.front();
+        q.pop();
+
+        for(auto it : v[no]){
+            if(dist[it] == -1){
+                dist[it] = dist[no]+1;
+                q.push(it);
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    vector<vector<int> > v(n+1);
+
+    int l, r;
+    for(int i = 0; i != n; i++){
+        l = r = i;
+        while(l >= 0 && r < n && s[l] == s[r]){
+            v[l].push_back(r+1);
+            l--;
+            r++;
+        }
+    }
+    for(int i = 0; i < n-1; i++){
+        l = i;
+        r = i+1;
+
+        while(l >= 0 && r < n && s[l] == s[r]){
+            v[l].push_back(r+1);
+            l--;
+            r++;
+        }
+    }
+
+    vector<int> dist(n+1, -1);
+    bfs(v, 0, dist);
+    cout << dist[n] << endl;
+}
