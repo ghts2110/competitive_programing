b11c3c052492253f323f3abab546eedebf3ece8a
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2024-02-02 22:45:58 -0300

    Turbo Sort

---
+++ spoj/Turbo Sort/Turbo_Sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    vector<int> v(n);
+    for(int i = 0; i != n; i++){
+      scanf("%d", &v[i]);
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i != n; i++){
+      printf("%d\n", v[i]);
+    }
+}
