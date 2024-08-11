275d30e6b6e48fbf3d020f6f352a4cac062a325a
ghts2110 <gabrieltorres2110@gmail.com>
2024-08-10 21:59:28 -0300

    Shopaholic

---
+++ uva/Shopaholic/Shopaholic.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+        for(int i = 0; i != n; i++){
+            cin >> v[i];
+        }
+
+        sort(v.begin(), v.end());
+        int cont = 0;
+        for(int i = v.size()-3; i > -1; i -= 3){
+            cont += v[i];
+        }
+
+        cout << cont << endl;
+    }
+}
